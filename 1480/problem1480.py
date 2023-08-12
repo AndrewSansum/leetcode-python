35831c5cade143ee980bffae93e1aa93fb482cf8
Andrew Sansum <sansumandrew@gmail.com>
2023-08-12 12:18:08 +0100

    feat: complete problem 1480

--- 1480/problem1480.py
+++ 1480/problem1480.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        pass
+        if not nums:
+            raise ValueError("nums may not be empty")
+
+        sums = []
+        for n in nums:
+            if len(sums) < 1:
+                sums.append(n)
+            else:
+                sums.append(n + sums[-1])
+
+        return sums
