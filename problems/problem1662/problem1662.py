338203bbfa57c060ad471eb820c937b973ac1151
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 15:49:05 +0000

    feat: complete problem 1662

--- problems/problem1662/problem1662.py
+++ problems/problem1662/problem1662.py
@@ -3,4 +3,4 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return False
+        return "".join(word1) == "".join(word2)
