ad9743a8176325451303cd9fd6dc905ec7697bd0
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 16:34:36 +0000

    test: create basic test for prob 1160

--- problems/problem1160/problem1160_test.py
+++ problems/problem1160/problem1160_test.py
@@ -2,8 +2,18 @@ import unittest
 
 from problem1160 import Solution
 
+
 class Test1160Solution(unittest.TestCase):
-    pass
+    def test_example1(self):
+        words = ["cat", "bt", "hat", "tree"]
+        chars = "atach"
+        self.assertEqual(Solution().countCharacters(words, chars), 6)
+
+    def test_example2(self):
+        words = ["hello", "world", "leetcode"]
+        chars = "welldonehoneyr"
+        self.assertEqual(Solution().countCharacters(words, chars), 10)
+
 
 if __name__ == "__main__":
     unittest.main()
