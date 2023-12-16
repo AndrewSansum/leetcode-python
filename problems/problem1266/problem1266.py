dd0d83f73fdc7f675b389df82e17942b7440c0be
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 17:00:30 +0000

    feat: complete prob 1266

--- problems/problem1266/problem1266.py
+++ problems/problem1266/problem1266.py
@@ -3,4 +3,10 @@ from typing import List
 
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        pass
+        score = 0
+        for i in range(1, len(points)):
+            score += max(
+                abs(points[i][0] - points[i - 1][0]),
+                abs(points[i][1] - points[i - 1][1]),
+            )
+        return score
