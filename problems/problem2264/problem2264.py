a2525eb04ee4c4add26da7c367edf15e559e8269
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 17:18:34 +0000

    feat: complete problem 2264

--- problems/problem2264/problem2264.py
+++ problems/problem2264/problem2264.py
@@ -1,3 +1,11 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        pass
+        result = None
+        for i in range(2, len(num)):
+            if num[i - 2] == num[i - 1] and num[i - 1] == num[i]:
+                concatted = num[i - 2] + num[i - 1] + num[i]
+                if result is None or int(concatted) > int(result):
+                    result = concatted
+        if result is None:
+            result = ""
+        return result
