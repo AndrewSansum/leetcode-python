6dbd78407146ca3641693f965c1dabfd14e391bd
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 16:55:32 +0000

    test: setup tests for prob 1266

--- problems/problem1266/problem1266_test.py
+++ problems/problem1266/problem1266_test.py
@@ -2,8 +2,16 @@ import unittest
 
 from problem1266 import Solution
 
+
 class Test1266Solution(unittest.TestCase):
-    pass
+    def test_example1(self):
+        points = [[1, 1], [3, 4], [-1, 0]]
+        self.assertEqual(Solution().minTimeToVisitAllPoints(points), 6)
+
+    def test_example2(self):
+        points = [[3, 2], [-2, 2]]
+        self.assertEqual(Solution().minTimeToVisitAllPoints(points), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
