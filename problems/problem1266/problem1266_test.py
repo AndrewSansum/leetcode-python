75a557480816d76e7da6775fc55dd4dace35878f
Andrew Sansum <sansumandrew@gmail.com>
2023-12-16 16:59:38 +0000

    test: fix incorrect test case in example 1

--- problems/problem1266/problem1266_test.py
+++ problems/problem1266/problem1266_test.py
@@ -6,7 +6,7 @@ from problem1266 import Solution
 class Test1266Solution(unittest.TestCase):
     def test_example1(self):
         points = [[1, 1], [3, 4], [-1, 0]]
-        self.assertEqual(Solution().minTimeToVisitAllPoints(points), 6)
+        self.assertEqual(Solution().minTimeToVisitAllPoints(points), 7)
 
     def test_example2(self):
         points = [[3, 2], [-2, 2]]
