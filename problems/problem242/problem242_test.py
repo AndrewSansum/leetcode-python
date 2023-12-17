e7eeeb3045fbec0ea4e8a2823bd151eed2e3c283
Andrew Sansum <sansumandrew@gmail.com>
2023-12-17 13:13:32 +0000

    test: add example tests

--- problems/problem242/problem242_test.py
+++ problems/problem242/problem242_test.py
@@ -2,8 +2,18 @@ import unittest
 
 from problem242 import Solution
 
+
 class Test242Solution(unittest.TestCase):
-    pass
+    def test_example1(self):
+        s = "anagram"
+        t = "nagaram"
+        self.assertTrue(Solution().isAnagram(s, t))
+
+    def test_example2(self):
+        s = "rat"
+        t = "car"
+        self.assertFalse(Solution().isAnagram(s, t))
+
 
 if __name__ == "__main__":
     unittest.main()
