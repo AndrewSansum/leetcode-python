c9cc7acbd39412c6df7d1f3fb1e37a62d50969c4
Andrew Sansum <sansumandrew@gmail.com>
2023-12-17 13:30:56 +0000

    test: setup example tests

--- problems/problem1/problem1_test.py
+++ problems/problem1/problem1_test.py
@@ -2,8 +2,23 @@ import unittest
 
 from problem1 import Solution
 
+
 class Test1Solution(unittest.TestCase):
-    pass
+    def test_example1(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        self.assertListEqual(set(Solution().twoSum(nums, target)), set([0, 1]))
+
+    def test_example2(self):
+        nums = [3, 2, 4]
+        target = 6
+        self.assertListEqual(set(Solution().twoSum(nums, target)), set([1, 2]))
+
+    def test_example3(self):
+        nums = [3, 3]
+        target = 6
+        self.assertSetEqual(set(Solution().twoSum(nums, target)), set([0, 1]))
+
 
 if __name__ == "__main__":
     unittest.main()
